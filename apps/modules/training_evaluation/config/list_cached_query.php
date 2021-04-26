26f29bc9f26a28f7cec430dabaedc7aaf8fdab9c
RFC <tgarcia@softecdigital.com>
2021-04-26 13:16:07 +0800

    training evaluation

--- apps/modules/training_evaluation/config/list_cached_query.php
+++ apps/modules/training_evaluation/config/list_cached_query.php
@@ -2,20 +2,20 @@
 $config["list_cached_query"] = 'SELECT 
 `ww_training_calendar`.`training_calendar_id` as record_id,
 `ww_training_calendar`.`topic` as training_calendar_topic, 
-`t3`.`training_course` as "training_course",
-CONCAT( `t1`.`course` ) AS training_subject, 
+`T3`.`training_course` as "training_course",
+CONCAT( `T1`.`course` ) AS training_subject, 
 `ww_training_calendar`.`training_calendar_id`, 
 `ww_training_feedback`.`feedback_id`,
-`t3`.`start_date` as `start_date`,
-`t3`.`end_date` as `end_date`,
-`t2`.`sessiontime_from` as `sessiontime_from`,
-`t2`.`sessiontime_to` as `sessiontime_to`,
-`t2`.`instructor` as `instructor`
+`T3`.`start_date` as `start_date`,
+`T3`.`end_date` as `end_date`,
+`T2`.`sessiontime_from` as `sessiontime_from`,
+`T2`.`sessiontime_to` as `sessiontime_to`,
+`T2`.`instructor` as `instructor`
 FROM (`ww_training_calendar`)
-LEFT JOIN `ww_training_calendar_session` t2 ON `t2`.`training_calendar_id`=`ww_training_calendar`.`training_calendar_id`
-LEFT JOIN `ww_training_employee_database` t3 ON `t3`.`training_calendar_id`=`ww_training_calendar`.`training_calendar_id`
-LEFT JOIN `ww_training_course` t1 ON `t1`.`course_id`=`ww_training_calendar`.`course_id`
+LEFT JOIN `ww_training_calendar_session` T2 ON `T2`.`training_calendar_id`=`ww_training_calendar`.`training_calendar_id`
+LEFT JOIN `ww_training_employee_database` T3 ON `T3`.`training_calendar_id`=`ww_training_calendar`.`training_calendar_id`
+LEFT JOIN `ww_training_course` T1 ON `T1`.`course_id`=`ww_training_calendar`.`course_id`
 LEFT JOIN `ww_training_feedback` ON `ww_training_feedback`.`training_calendar_id` = `ww_training_calendar`.`training_calendar_id`
-LEFT JOIN `ww_training_calendar_participant` t4 ON `t4`.`training_calendar_id`=`ww_training_calendar`.`training_calendar_id`
+LEFT JOIN `ww_training_calendar_participant` T4 ON `T4`.`training_calendar_id`=`ww_training_calendar`.`training_calendar_id`
 WHERE (`ww_training_calendar`.`closed` =  1)
 ';
