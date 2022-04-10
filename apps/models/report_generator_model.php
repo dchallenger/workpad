9c9375c8c2f902f8666d4c836ce36babdb5fe9ee
Tirso Garcia Jr <tjkgarcia@yahoo.com>
2022-04-10 10:00:22 +0800

    Update report_generator_model.php

--- apps/models/report_generator_model.php
+++ apps/models/report_generator_model.php
@@ -875,10 +875,10 @@ class report_generator_model extends Record
 			$content->getActiveSheet()->getColumnDimension('J')->setAutoSize(true);
 			$content->getActiveSheet()->getColumnDimension('K')->setAutoSize(true);
 
-			$content->getActiveSheet()->getStyle("A1:K1")->applyFromArray($style_center);
+			$content->getActiveSheet()->getStyle("A1:K2")->applyFromArray($style_center);
 			$content->getActiveSheet()->mergeCells('A1:K1');
 
-			$content->getActiveSheet()->getStyle("A1:K1")->getFont()->setBold(true);
+			$content->getActiveSheet()->getStyle("A1:K2")->getFont()->setBold(true);
 
 			$content->getActiveSheet()->getStyle("A2:K".($result->num_rows()+2))->applyFromArray($border_style);
 		}
